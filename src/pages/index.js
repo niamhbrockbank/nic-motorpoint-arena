eb87c926927602c8ea626444f1b8e08b133f3fd4
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-13 17:04:07 +0000

    Format with prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import EventList from "./EventList.jsx"
+import EventList from "./EventList.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
