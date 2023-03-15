f3c5b1c07aed079617483ff5d959e88e24e95c13
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-15 17:12:26 +0000

    Sort event list by closest date

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "@/styles/Home.module.css";
+import Event from "./Event"
 
 export default function EventList() {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
@@ -12,13 +13,17 @@ export default function EventList() {
           `https://staging.national-ice-centre.com/api/events/read?token=${apiKey}`
         );
         const jsonBody = await response.json();
-        setEvents(jsonBody.events);
+        const nextEvents = jsonBody.events.map(event => {return {...event, id : crypto.randomUUID()}})
+
+        setEvents(nextEvents);
       } catch (error) {
         console.error(error);
       }
     }
     fetchEvents();
   }, []);
+
+  const sortedEvents = events.sort((event1 , event2) => sortByDate(event1, event2))
 
   return (
     <>
@@ -27,15 +32,23 @@ export default function EventList() {
         <p>Making space for culture and shared experience.</p>
       </div>
       <div className={styles.grid}>
-        {events.map((event, i) => (
-          <div key={i} className={styles.card}>
-            <span>
-              <h2>{event.name}</h2>
-              <p>description</p>
-            </span>
-          </div>
+        {sortedEvents.map((event, i) => (
+          <Event key={i} event={event} />
         ))}
       </div>
     </>
   );
+}
+
+function sortByDate(a, b){
+  const firstDateA = a.dates[0].date
+  const firstDateB = b.dates[0].date
+
+  if (firstDateA < firstDateB){
+    return -1
+  } else if (firstDateB < firstDateA){
+    return 1
+  }
+
+  return 0
 }
