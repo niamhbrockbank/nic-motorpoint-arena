02cc5908a7f1644de0d2675c9ee0a72b9309053e
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-16 16:07:15 +0000

    Increase width to fill card

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,7 +5,7 @@ export default function Event({ event }) {
   return (
     <div className={styles.card}>
       <span onClick={() => window.open(event.url, "_blank")}>
-        <Image src={event.image} alt={event.name} width="200" height="100" />
+        <Image src={event.image} alt={event.name} width="220" height="100" />
         <h2>{event.name}</h2>
         <p>{event.dates ? event.dates[0].date : "TBC"}</p>
       </span>
