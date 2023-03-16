20373a033aeb84d1a3601c8faedb986d2d7e6472
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-16 15:43:43 +0000

    Use event image and url properties from API

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,9 +3,9 @@ import styles from "../styles/Home.module.css";
 export default function Event({ event }) {
   return (
     <div className={styles.card}>
-      <span>
+      <span onClick={() => window.open(event.url, "_blank")}>
+      <img src={event.image} alt={event.name} width="200" height="100"/>
         <h2>{event.name}</h2>
-        <p>description</p>
         <p>{event.dates ? event.dates[0].date : "TBC"}</p>
       </span>
     </div>
