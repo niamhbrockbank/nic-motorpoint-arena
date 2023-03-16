11630daa233d744f2769556ccdc44ecc4c225d78
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-16 16:04:18 +0000

    Use next/image to optimise images

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,10 +1,11 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Event({ event }) {
   return (
     <div className={styles.card}>
       <span onClick={() => window.open(event.url, "_blank")}>
-        <img src={event.image} alt={event.name} width="200" height="100" />
+        <Image src={event.image} alt={event.name} width="200" height="100" />
         <h2>{event.name}</h2>
         <p>{event.dates ? event.dates[0].date : "TBC"}</p>
       </span>
