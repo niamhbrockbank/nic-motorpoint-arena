c290eab3986b1c833cc40ad44ba234a239d9b140
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-16 17:03:57 +0000

    Using inter font through next

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,6 +1,9 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import moment from "moment";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Event({ event }) {
   const { name, url, image, dates } = event;
@@ -8,7 +11,7 @@ export default function Event({ event }) {
   const formattedDates = dates.map((d) => moment(d.date).format("Do MMM YYYY"));
 
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${inter.className}`}>
       <span onClick={() => window.open(url, "_blank")}>
         <Image
           src={image ? image : "/./images/loading.jpeg"}
@@ -16,7 +19,7 @@ export default function Event({ event }) {
           width="220"
           height="100"
         />
-        <h2>{name}</h2>
+        <h3>{name}</h3>
         <p>
           {dates.length > 1
             ? `${formattedDates[0]} - ${formattedDates[dates.length - 1]}`
