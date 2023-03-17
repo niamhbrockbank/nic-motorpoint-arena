d6a7d066259418e682ad92b54719aff9f908c824
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-17 12:22:54 +0000

    Convert dates to a readable sentence

---
+++ src/utils/describeDates.js
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+/**
+ * Converts an array of dates into human readable description of dates
+ * @param {string[]} dateArr Array of date strings
+ * @returns {string} Description of the dates available
+ */
+export default function describeDates(dateArr){
+    const numOfDates = dateArr.length
+    const longFormDates = dateArr.map((d) => moment(d.date).format("Do MMMM YYYY"));
+  
+    if (numOfDates === 1){
+      return longFormDates[0]
+    } else if (numOfDates === 2){
+      return `${longFormDates[0]} & ${longFormDates[1]}`
+    } 
+  
+    return `${longFormDates[0]} - ${longFormDates[numOfDates - 1]}`
+  }
