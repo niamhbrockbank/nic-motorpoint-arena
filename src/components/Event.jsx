fcb016b6365803933e406b819b2177ba3493ac53
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-17 14:01:08 +0000

    Style event cards

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,30 +1,42 @@
 import styles from "../styles/Home.module.scss";
 import Image from "next/image";
-import moment from "moment";
 import { Inter } from "next/font/google";
+import describeDates from "@/utils/describeDates";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Event({ event }) {
   const { name, url, image, dates } = event;
 
-  const formattedDates = dates.map((d) => moment(d.date).format("Do MMM YYYY"));
+  const datesDescription = describeDates(dates);
 
   return (
     <div className={`${styles.card} ${inter.className}`}>
+      <Image
+        src={image ? image : "/./images/loading.jpeg"}
+        alt={name}
+        width="220"
+        height="100"
+      />
       <span onClick={() => window.open(url, "_blank")}>
-        <Image
-          src={image ? image : "/./images/loading.jpeg"}
-          alt={name}
-          width="220"
-          height="100"
-        />
-        <h3>{name}</h3>
-        <p>
-          {dates.length > 1
-            ? `${formattedDates[0]} - ${formattedDates[dates.length - 1]}`
-            : formattedDates[0]}
-        </p>
+        <h2>{name}</h2>
+        <p>{datesDescription}</p>
+        <div className={styles.link}>
+          <p>VIEW EVENT</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+            />
+          </svg>
+        </div>
       </span>
     </div>
   );
