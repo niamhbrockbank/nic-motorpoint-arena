dd717f32f4f70a433373729dfb72faaf7521c3a8
niamhbrockbank <niamh.brockbank@gmail.com>
2023-03-17 13:59:14 +0000

    Format with prettier

--- src/utils/describeDates.js
+++ src/utils/describeDates.js
@@ -5,15 +5,17 @@ import moment from "moment";
  * @param {string[]} dateArr Array of date strings
  * @returns {string} Description of the dates available
  */
-export default function describeDates(dateArr){
-    const numOfDates = dateArr.length
-    const longFormDates = dateArr.map((d) => moment(d.date).format("Do MMMM YYYY"));
-  
-    if (numOfDates === 1){
-      return longFormDates[0]
-    } else if (numOfDates === 2){
-      return `${longFormDates[0]} & ${longFormDates[1]}`
-    } 
-  
-    return `${longFormDates[0]} - ${longFormDates[numOfDates - 1]}`
+export default function describeDates(dateArr) {
+  const numOfDates = dateArr.length;
+  const longFormDates = dateArr.map((d) =>
+    moment(d.date).format("Do MMMM YYYY")
+  );
+
+  if (numOfDates === 1) {
+    return longFormDates[0];
+  } else if (numOfDates === 2) {
+    return `${longFormDates[0]} & ${longFormDates[1]}`;
   }
+
+  return `${longFormDates[0]} - ${longFormDates[numOfDates - 1]}`;
+}
